cdabdb54560771934b749f17dedcdb52fcf31d1f
N8 <nathan.brachotte+github@gmail.com>
2024-09-14 06:57:48 -0400

    feat: 🎸 astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
+
+  adapter: cloudflare(),
 });
