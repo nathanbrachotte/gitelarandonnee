0bb3c7bed8029d60f5fff30d6d4fcfc171e44d0f
N8 <nathan.brachotte+github@gmail.com>
2024-09-17 14:46:16 -0400

    feat: 🎸 add font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        logo: ["Campfire", "sans-serif"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
