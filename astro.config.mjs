8e65950a5c9aca78ce2d2568a5740346c0ce4760
N8 <nathan.brachotte+github@gmail.com>
2024-10-14 00:18:02 +0200

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gitelarandonnee.fr",
   output: "hybrid",
 
   integrations: [
+    sitemap(),
     react(),
     tailwind({
       applyBaseStyles: false,
